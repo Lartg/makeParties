562530694861774e3bfed153e54abdfee6046fe1
Lartg <roc.gerrety@gmail.com>
2022-03-24 10:44:04 -0700

    ill get the hang of this eventually

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-const express = require('express')
-const app = express()
+// Require Libraries
+const fetch = import('node-fetch');
+const express = require('express');
+
+
+// App Setup
+const app = express();
+
+// Middleware
+const { engine } = require('express-handlebars');
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.send('Hello World')
