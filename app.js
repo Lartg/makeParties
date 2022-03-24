77aa03213a937bd24606153db7305eab7a768f8c
Lartg <roc.gerrety@gmail.com>
2022-03-24 11:36:22 -0700

    connected to sqlite3 in memory, cant figure out postgres too well, ill ask dylan or elias later

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 // Require Libraries
 const fetch = import('node-fetch');
 const express = require('express');
+const bodyParser = require('body-parser');
 
+//db
+const { Sequelize } = require('sequelize')
+// // Option 1: Passing a connection URI
+const sequelize = new Sequelize('sqlite::memory:') // Example for sqlite
+// const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname') // Example for postgres
 
+// // Option 2: Passing parameters separately (sqlite)
+// const sequelize = new Sequelize({
+//   dialect: 'sqlite',
+//   storage: 'path/to/database.sqlite'
+// });
+
+// Option 3: Passing parameters separately (other dialects)
+// const sequelize = new Sequelize('events-tutorial', '1', '1', {
+//   host: 'localhost',
+//   dialect: 'postgres'
+// });
 // App Setup
 const app = express();
 
@@ -13,6 +30,15 @@ app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+sequelize.authenticate()
+ .then(() => {
+   console.log('Connection has been established successfully.');
+ })
+ .catch(err => {
+   console.error('Unable to connect to the database:', err);
+ });
 
 let events = [
   { title: "I am your first event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
@@ -24,8 +50,14 @@ app.get('/', (req, res) => {
   res.render('events-index', {events: events});
 })
 
+app.get('/events/new', (req, res) => {
+  res.render('events-new', {});
+})
 
-
+// CREATE
+app.post('/events', (req, res) => {
+  console.log(req.body);
+})
 
 
 
